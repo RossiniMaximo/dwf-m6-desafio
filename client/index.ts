4d264e1b79b49a6fa25a1fd4c18a9569f14fce56
Maximo <maximorossini2016@gmail.com>
2021-12-15 20:04:14 -0300

    seventh commit

--- client/index.ts
+++ client/index.ts
@@ -3,7 +3,9 @@ import "./components/hands";
 import "./components/timer";
 import "./components/star";
 import { initRouter } from "./router";
+import { state } from "./state";
 (function () {
   const rootEl = document.querySelector(".root");
   initRouter(rootEl);
+  state.initStorage();
 })();
